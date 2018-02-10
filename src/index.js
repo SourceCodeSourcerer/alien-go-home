60216099e46c03f19ef29529d1a532e9bc9417bc
Peter Lagerhem <peter@lagerhem.com>
2018-02-10 18:13:22 +0100

    First running part done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
+import Game from './containers/Game';
+import reducer from './reducers';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+    reducer, /* preloadState, */
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), 
+);
+/* eslint-enable */
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Game />
+    </Provider>, 
+    document.getElementById('root'),
+);
 registerServiceWorker();
